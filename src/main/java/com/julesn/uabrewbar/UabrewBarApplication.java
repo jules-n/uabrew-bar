28fb50433cfe4e9f9ea9d31cafa2c2253927ceda
jules <neroba.yulia@gmail.com>
2022-06-16 10:12:39 +0300

    init commit

---
+++ src/main/java/com/julesn/uabrewbar/UabrewBarApplication.java
@@ -0,0 +1,13 @@
+package com.julesn.uabrewbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UabrewBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UabrewBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julesn/uabrewbar/UabrewBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julesn.uabrewbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UabrewBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
