341680d36adce87a71680d93d6a9675682ffddd3
Jules <73499895+jules-n@users.noreply.github.com>
2022-06-22 14:19:38 +0300

    Create Order.java

---
+++ src/main/java/com/julesn/uabrewbar/domain/Order.java
@@ -0,0 +1,28 @@
+package com.julesn.uabrewbar.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Document(collection = Order.COLLECTION)
+public class Order {
+    public static final String COLLECTION = "orders";
+    private String _id;
+    @Indexed(unique = true)
+    private int number;
+    private STATUS status;
+    private String place;
+    private String bar;
+    private Set<String> rights;
+    private Map<String, Integer> positions;
+}
