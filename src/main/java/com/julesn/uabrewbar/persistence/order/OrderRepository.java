6eaa922d3b491828c982900a16050346b8fe9410
Jules <73499895+jules-n@users.noreply.github.com>
2022-06-22 14:49:14 +0300

    Update and rename src/main/java/com/julesn/uabrewbar/persistence/order.OrderRepository.java to src/main/java/com/julesn/uabrewbar/persistence/order/OrderRepository.java

---
+++ src/main/java/com/julesn/uabrewbar/persistence/order/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.julesn.uabrewbar.persistence.order;
+
+import com.julesn.uabrewbar.domain.Order;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends MongoRepository<Order, String> {
+}
